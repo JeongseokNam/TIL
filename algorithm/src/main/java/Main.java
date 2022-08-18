169af5701cb7c4d1ce31d5f198328a435337e8d7
JeongSeokNam <wlqlel00@naver.com>
2022-08-18 17:42:50 +0900

    today algorithm

--- algorithm/src/main/java/Main.java
+++ algorithm/src/main/java/Main.java
@@ -1,11 +1,15 @@
 import programmers.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("알고리즘 테스트 시작");
 
-        같은_숫자는_싫어 s = new 같은_숫자는_싫어();
-        int[] arr = {1,1,3,3,0,1,1};
+        소수_만들기 s = new 소수_만들기();
+        int[] arr = {1,2,7,6,4};
+
         System.out.println("결과 : "+s.solution(arr));
         System.out.println("알고리즘 테스트 끝");
     }
---
+++ algorithm/src/main/java/programmers/소수_만들기.java
@@ -0,0 +1,24 @@
+package programmers;
+
+public class 소수_만들기 {
+    public int solution(int[] nums) {
+        int answer = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i+1; j < nums.length; j++) {
+                for (int k = j+1; k < nums.length; k++) {
+                    int tempNum = nums[i]+nums[j]+nums[k];
+                    int cnt = 0;
+                    for (int l = 0; l < tempNum; l++) {
+                        if (tempNum%(l+1)==0){
+                            cnt++;
+                        }
+                    }
+                    if (cnt==2){
+                        answer++;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+}
