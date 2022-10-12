265696cf1c9ed9cdfab4c413cb31017333db6231
DESKTOP-AMH2PNG\user <wlqlel00@naver.com>
2022-10-12 10:11:46 +0900

    알고리즘 2022 10 12 -2

---
+++ algorithm/src/main/java/programmers/두_수의_나눗셈.java
@@ -0,0 +1,8 @@
+package programmers;
+
+public class 두_수의_나눗셈 {
+    public int solution(int num1, int num2) {
+        int answer = (int)(((double)num1/(double)num2)*1000);
+        return answer;
+    }
+}
---
+++ algorithm/src/main/java/programmers/배열의_평균값.java
@@ -0,0 +1,11 @@
+package programmers;
+
+public class 배열의_평균값 {
+    public double solution(int[] numbers) {
+        double answer = 0;
+        for(int i=0; i<numbers.length; i++){
+            answer +=numbers[i];
+        }
+        return answer/numbers.length;
+    }
+}
